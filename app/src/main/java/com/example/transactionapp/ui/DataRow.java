964f872ea157cbebf88d4cb19dcd4deb04f9295c
Mrhube <66394761+Mrhube@users.noreply.github.com>
2020-07-06 20:19:21 -0500

    UI table cleanup
    Fixed weights on HeaderRow and DataRow and added comments

--- app/src/main/java/com/example/transactionapp/ui/DataRow.java
+++ app/src/main/java/com/example/transactionapp/ui/DataRow.java
@@ -7,21 +7,34 @@ import android.widget.TextView;
 
 public class DataRow extends TableRow {
 
+    /**
+     * id is an integer used to control the alternating color scheme of the table rows
+     * selected is flag used to indicate whether this row has been clicked by the user
+     */
     public int id;
-    Boolean expanded = false;
+    Boolean selected = false;
 
+    /**
+     * Constructor for DataRow
+     * @param context - the application context
+     * @param id - integer used to determine row shading, should match the index of the row within the table
+     * @param date - the data string to display
+     * @param amount - the amount string to display, without the currency symbol
+     * @param category - the category string to display
+     */
     public DataRow(Context context, int id, String date, String amount, String category) {
         super(context);
         this.id = id;
         this.setBackgroundColor(Color.parseColor(getColor()));
         this.setLayoutParams(new TableRow.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 1));
 
-        String text[] = {date,amount,category};
-        Float weight[] = {1F,2F,2F};
+        String text[] = {date,"$" + amount,category};
+        Float weight[] = {0.3F,0.3F,0.4F};
 
         for (int i = 0; i < 3; i++) {
             TextView view = new TextView(context);
-            view.setLayoutParams(new TableRow.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, weight[i]));
+            TableRow.LayoutParams params = new TableRow.LayoutParams();
+            view.setLayoutParams(new TableRow.LayoutParams(0, LayoutParams.WRAP_CONTENT, weight[i]));
             view.setPadding(10,10,10,10);
             view.setTextSize(15);
             view.setText(text[i]);
@@ -29,14 +42,21 @@ public class DataRow extends TableRow {
         }
     }
 
-    public void toggle() {
-        this.expanded = !expanded;
+    /**
+     * Toggles whether this row is selected or not and updates the background color accordingly
+     */
+    void toggle() {
+        this.selected = !selected;
         this.setBackgroundColor(Color.parseColor(getColor()));
     }
 
+    /**
+     * Determines what color this row should be
+     * @return a hex string representing a color
+     */
     private String getColor() {
-        if (expanded) return "#FFFF77";
-        else if (id % 2 == 1) return "#FFFFFF";
-        else return "#DAE8FC";
+        if (selected) return "#FFFF77"; // yellow
+        else if (id % 2 == 1) return "#FFFFFF"; // white
+        else return "#DAE8FC"; // light blue
     }
 }
--- app/src/main/java/com/example/transactionapp/ui/HeaderRow.java
+++ app/src/main/java/com/example/transactionapp/ui/HeaderRow.java
@@ -8,17 +8,21 @@ import android.widget.TextView;
 
 public class HeaderRow extends TableRow {
 
+    /**
+     * Constructor for HeaderRow
+     * @param context - the application context
+     */
     public HeaderRow(Context context) {
         super(context);
         this.setBackgroundColor(Color.parseColor("#0079D6"));
         this.setLayoutParams(new TableRow.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 1));
 
         String text[] = {"Date","Amount","Category"};
-        Float weight[] = {1F,2F,2F};
+        Float weight[] = {0.3F,0.3F,0.4F};
 
         for (int i = 0; i < 3; i++) {
             TextView view = new TextView(context);
-            view.setLayoutParams(new TableRow.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, weight[i]));
+            view.setLayoutParams(new TableRow.LayoutParams(0, LayoutParams.WRAP_CONTENT, weight[i]));
             view.setPadding(10,10,10,10);
             view.setTypeface(view.getTypeface(), Typeface.BOLD);
             view.setTextColor(Color.parseColor("#FFFFFF"));
